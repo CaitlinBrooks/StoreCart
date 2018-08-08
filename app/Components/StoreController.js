93d942ce664cc055aaac7e4e0c60c77787cf3c2c
CaitlinCodes <caiit@me.com>
2018-08-08 15:26:10 -0600

    tinkering

--- app/Components/StoreController.js
+++ app/Components/StoreController.js
@@ -1,3 +1,17 @@
 import "./StoreService.js"
 
+// function updateItems() {
+//   let items = StoreService.getItems()
+//   draw the items to the page
+//   let template = ''
+//   for (let i = 0; i < items.length; i++) {
+//     const item = items[i];
+//     template += `
+//             <div class="store-item" onclick="app.controllers.StoreController.vend(${i})">
+//               <img src="${item.img}" alt = "">
+//               <h3> ${item.price}</h3> 
+//             </div>
+//             `
+//   }
+
 console.log("hello from StoreController.js")
---
+++ app/Models/Store.js
@@ -0,0 +1,29 @@
+console.log("hello from Store.js")
+
+class Store {
+  constructor() {
+    this.subTotal = this.subTotal
+    //   this.acceptablePayment = {
+    //     creditCard: creditCard,
+    // }
+    this.shoppingCart = this.shoppingCart
+    this.taxTotal = this.taxTotal
+    this.transactionTotal = this.transactionTotal
+    this.storeItems = [{
+      id: 'socks',
+      img: 'http://cdn.shopify.com/s/files/1/1119/5850/products/grey-and-ocean-blue-grid_85bcb20f-167e-4142-a2c2-56eedce6ffe1_200x200.jpg?v=1529333314',
+      price: 8.00,
+      quantity: 10
+    }, {
+      id: 'hairbrush',
+      img: 'https://images-na.ssl-images-amazon.com/images/I/41zgYBEHIyL._SY355_.jpg',
+      price: 6.00,
+      quantity: 20
+    }, {
+      id: 'bottle',
+      img: 'https://www.sparkbrs.com/merchant/4007/images/zoom/hydro-flask-stainless-steel-vacuum-insulated-water-bottle-32-oz-wide-mouth-straw-lid-mint.jpeg',
+      price: 40.00,
+      quantity: 5
+    }]
+  }
+}
